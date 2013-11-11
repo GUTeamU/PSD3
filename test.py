b3a10359b911be4c9fe4af0f64ef70fd1f25af98
Edvin Malinovskis <edvin.malinovskis@gmail.com>
2013-11-11 11:30:57 +0000

    Added the test file

---
+++ test.py
@@ -0,0 +1,15 @@
+import os
+import cats
+import unittest
+import tempfile
+
+class CatsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
